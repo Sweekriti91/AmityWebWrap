97338be8a9382a9928a6c5e7f6d1460813101ec2
Sweekriti Satpathy <swsat@microsoft.com>
2024-02-23 11:21:15 -0800

    change divs based on function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import "./App.css";
 
-import { AmityUiKitFeed, AmityUiKitProvider, AmityUIKitCommunitySideSection } from "@amityco/ui-kit-open-source";
-
+// import { AmityUiKitFeed, AmityUiKitProvider, AmityUIKitCommunitySideSection } from "@amityco/ui-kit-open-source";
+import { AmityUiKitFeed, AmityUiKitProvider, AmityUiKitSocial, AmityUIKitCommunityList, AmityUIKitCommunitySideSection } from "@amityco/ui-kit-open-source";
 const apiKey = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
 
 export default function App() {
   return (
     <div className="App" styles={{ height: '500px', overflowY: 'scroll' }} >
-        <AmityUiKitProvider
-          key="test"
-          apiKey={apiKey}
-          userId="test"
-          displayName="test"
-        >
-
-        <AmityUiKitFeed/>
-        {/* <AmityUIKitCommunitySideSection/> */}
-
-        </AmityUiKitProvider>
+      <div id="testButtonGroup" onClick={() => setAmityTabView("group")} className={"test-button"}>GROUP BUTTON</div>
+      <div id="testButtonNews" onClick={() => setAmityTabView("news")} className={"test-button"}>NEWS BUTTON</div>
       
+      <AmityUiKitProvider
+               key="test"
+               apiKey={apiKey}
+               userId="test"
+               displayName="test">
+        <div id="amityAmpasNewsFeedContainer">
+          <AmityUiKitFeed/>
+        </div>
+        <div id="amityAmpasGroupContainer">
+          <AmityUIKitCommunityList className="community-list"
+        communitiesQueryParam={{ membership: 'member', limit: 20 }}
+        activeCommunity="community"/> 
+        </div>
+      </AmityUiKitProvider>
     </div>
   );
+}
+
+window.testValSetData = function(val)
+{
+  setAmityTabView(val.toString());
+  // console.log("CALLING FROM .NET SET PAGE TO:" + val.ToString());
+}
+
+function setAmityTabView(typeName) {
+  var newsFeedViewContainer = document.getElementById("amityAmpasNewsFeedContainer"); // any container element id for news feed page
+  var groupViewContainer = document.getElementById("amityAmpasGroupContainer"); // any container element id for group page
+  if (newsFeedViewContainer && groupViewContainer) {
+      // set by type name parameter
+      switch (typeName) {
+          case "news":
+              // load/show news feed page and hide group page
+              if (groupViewContainer) {
+                  groupViewContainer.style.display = "none";
+              }
+              if (newsFeedViewContainer) {
+                  newsFeedViewContainer.style.display = "";
+              }
+              break;
+          case "group":
+              // load/show group page and hide news feed page
+              if (newsFeedViewContainer) {
+                  newsFeedViewContainer.style.display = "none";
+              }
+              if (groupViewContainer) {
+                  groupViewContainer.style.display = "";
+              }
+              break;
+          default:
+              // hide both pages
+              if (newsFeedViewContainer) {
+                  newsFeedViewContainer.style.display = "none";
+              }
+              if (groupViewContainer) {
+                  groupViewContainer.style.display = "none";
+              }
+              break;
+      }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,37 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CommunityPage from './CommunityPage';
-import Login from './Login';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route
-} from "react-router-dom";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "feed",
-    element: <App />,
-  },
-  {
-    path: "community",
-    element: <CommunityPage />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
-      {/* <Route path="/" element={<App />} /> */}
+    <App />
    </React.StrictMode>
 );
 
